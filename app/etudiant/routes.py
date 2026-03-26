2b80d2634b55a95f1cbd5746e0a4a744e2cdfea1
alt-then-F4 <n.houles@hotmail.com>
2026-03-26 22:02:59 +0000

    clear

--- app/etudiant/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-etudiant_bp = Blueprint('etudiant', __name__)
-
-@etudiant_bp.route('/')
-def index():
-    return 'Etudiant dashboard - coming soon'
--- app/prof/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-prof_bp = Blueprint('prof', __name__)
-
-@prof_bp.route('/')
-def index():
-    return 'Prof dashboard - coming soon'
