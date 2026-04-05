a7279e4bb002619646fc17cbb43f30d232863f0c
altF4 <126264097+alt-then-F4@users.noreply.github.com>
2026-04-06 00:08:31 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1668,4 +1668,4 @@ def service_unavailable_error(error):
     ), 503
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False)  # nosec B104
